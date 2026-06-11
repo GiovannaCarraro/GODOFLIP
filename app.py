c544c80081783fa1348fd939aedf66c176203559
Gabrielli Michachi <gabrielli.paula@aluno.senai.br>
2026-06-11 14:49:46 -0300

    add: fiz a rota do pagina inicial

--- app.py
+++ app.py
@@ -3,5 +3,14 @@ from flask import Flask, render_template, redirect, request, session, jsonify
 app = Flask(__name__)
 
 @app.route("/")
-def pagina_inicial():
+def pagina_index():
     return render_template ("/")
+
+# pagina inicial 
+@app.route("pagina_inicial")
+def pagina_inicial():
+    return render_template("pag_inicial.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
