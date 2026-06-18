f123eca6cac576b1e337f7537bbdf3b0e5dac8b9
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2026-06-18 17:21:21 -0300

    fazendo o model

--- model/skate.py
+++ model/skate.py
@@ -70,3 +70,28 @@ def listar_banners():
     conexao.close()
 
     return banners
+
+
+def listar_pecas():
+    conexao, cursor = conectar()
+
+    sql = """
+    SELECT
+        p.cod_produto,
+        p.nome,
+        p.desc_produto,
+        p.preco,
+        i.url
+    FROM produtos p
+    INNER JOIN img_produtos i
+        ON p.cod_produto = i.produto_id
+    WHERE p.categoria = 'pecas'
+    """
+
+    cursor.execute(sql)
+    produtos = cursor.fetchall()
+
+    cursor.close()
+    conexao.close()
+
+    return produtos
