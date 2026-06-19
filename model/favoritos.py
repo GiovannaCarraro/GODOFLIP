2b1537eeed233b5ea5d9c83b87e830865c025840
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2026-06-19 08:16:27 -0300

    arrumando a parte de favoritos

--- model/favoritos.py
+++ model/favoritos.py
@@ -1,44 +1,40 @@
 from database.conexao import conectar
 
-def listar_favoritos(usuario_id):
+def listar_favoritos(id_usuario):
     conexao, cursor = conectar()
-
-    # Adicione esta linha para podermos usar texto no HTML
     cursor = conexao.cursor(dictionary=True)
 
+    # O segredo está no MIN(i.url) e no GROUP BY no final
     sql = """
-    SELECT
+    SELECT 
         p.cod_produto,
         p.nome,
         p.preco,
-        i.url
+        p.categoria,
+        MIN(i.url) AS url
     FROM favoritos f
-    INNER JOIN produtos p
-        ON f.produto_id = p.cod_produto
-    INNER JOIN img_produtos i
-        ON p.cod_produto = i.produto_id
+    INNER JOIN produtos p ON f.produto_id = p.cod_produto
+    INNER JOIN img_produtos i ON p.cod_produto = i.produto_id
     WHERE f.usuario_id = %s
+    GROUP BY p.cod_produto
     """
-
-    cursor.execute(sql, (usuario_id,))
+    
+    cursor.execute(sql, (id_usuario,))
     favoritos = cursor.fetchall()
 
     cursor.close()
     conexao.close()
-
+    
     return favoritos
 
-def adicionar_favorito(usuario_id, produto_id):
+def adicionar_favorito(id_usuario, id_produto):
     conexao, cursor = conectar()
-
     
-    sql = """
-    INSERT IGNORE INTO favoritos (usuario_id, produto_id)
-    VALUES (%s, %s)
-    """
-
-    cursor.execute(sql, (usuario_id, produto_id))
+    sql = "INSERT INTO favoritos (usuario_id, produto_id) VALUES (%s, %s)"
+    cursor.execute(sql, (id_usuario, id_produto))
+    
+    # SE FALTAR ESSA LINHA, O BANCO NÃO SALVA! 🔴
     conexao.commit() 
-
+    
     cursor.close()
     conexao.close()
